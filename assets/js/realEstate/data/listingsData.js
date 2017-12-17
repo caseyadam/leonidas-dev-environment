bee266a4ea44abf5aceb644cad1f4966f2bc8273
Casey Adam <caseymoths@Caseys-MacBook-Pro.local>
2017-12-17 09:07:07 -0800

    Minor Updates
    Slight tweaks to .js files and content area css

---
+++ assets/js/realEstate/data/listingsData.js
@@ -0,0 +1,102 @@
+var listingsData = [
+  {
+    address: '20-34 Grand Ave',
+    city: 'Ridgewood',
+    state: 'NY'
+    rooms: 3,
+    price: 220000,
+    floorSpace: 2000,
+    extras: [
+      'elevator',
+      'pool'
+    ],
+    homeType: 'Apartment'
+    image: 'http://media.equityapartments.com/images/c_crop,x_0,y_0,w_1920,h_1080/c_fill,w_1920,h_1080/q_80/4206-28/the-kelvin-apartments-exterior.jpg'
+  },
+  {
+    address: '987 Happy Mickey Fun Time Blvd',
+    city: 'Orlando',
+    state: 'FL'
+    rooms: 2,
+    price: 24666,
+    floorSpace: 1430,
+    extras: [
+      'elevator',
+      'gym'
+    ],
+    homeType: 'Apartment'
+    image: 'https://www.adinahotels.com/wp-content/uploads/sites/4/2016/07/adina-melbourne-flinders-street-apartment-hotel-one-and-two-bedroom-apartment-2-2013.jpg'
+  },
+  {
+    address: '37 W. Easy Street',
+    city: 'Richmond',
+    state: 'VA'
+    rooms: 3,
+    price: 343555,
+    floorSpace: 2400,
+    extras: [
+      'elevator',
+      'gym'
+    ],
+    homeType: 'Apartment'
+    image: 'https://c.o0bg.com/rf/image_960w/Boston/2011-2020/2017/01/09/BostonGlobe.com/Lifestyle/Images/doherty_10names04_liv.jpg'
+  },
+  {
+    address: '123 Fake Street',
+    city: 'Springfield',
+    state: 'IL'
+    rooms: 3,
+    price: 800000,
+    floorSpace: 1000,
+    extras: [
+      'elevator',
+      'gym'
+    ],
+    homeType: 'Apartment'
+    image: 'https://www.simpsonpropertygroup.com/Uploads2/CommunityAlertImages_resized/az160713-0947master3_1.jpg'
+  },
+  {
+    address: '2345 Pinecone Lane',
+    city: 'Redwood City',
+    state: 'CA'
+    rooms: 3,
+    price: 603000,
+    floorSpace: 3000,
+    extras: [
+      'elevator',
+      'gym'
+    ],
+    homeType: 'Condo'
+    image: 'https://2u0yuyu2fs02fa819ut739hy-wpengine.netdna-ssl.com/wp-content/uploads/2017/04/WestVillage-p14.jpg'
+  },
+  {
+    address: 'Tooling St',
+    city: 'San Diego',
+    state: 'CA'
+    rooms: 3,
+    price: 603000,
+    floorSpace: 3000,
+    extras: [
+      'elevator',
+      'gym'
+    ],
+    homeType: 'Apartment'
+    image: 'https://www.adinahotels.com/wp-content/uploads/sites/4/2016/09/adina-apartment-hotel-south-yarra-melbourne-studio-apartment-bedroom-king-2016-1.jpg'
+  },
+  {
+    address: '2730 Cherrywood Drive',
+    city: 'New Berlin',
+    state: 'WI'
+    rooms: 1,
+    price: 105000,
+    floorSpace: 3000,
+    extras: [
+      'elevator',
+      'gym'
+    ],
+    homeType: 'Apartment'
+    image: 'https://cdn.freshome.com/wp-content/uploads/2016/03/architecture-modern-apartment-1-850x450.jpg'
+  },
+]
+
+export default listingsData;
---
+++ assets/js/realEstate/realEstate.js
@@ -0,0 +1,36 @@
+import React, { Component} from 'react'
+import ReactDOM from 'react-dom'
+import Header from './Header.js'
+import Filter from './Filter.js'
+import Listings from './Listings.js'
+import listingsData from './data/listingsData.js'
+
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      name: 'Joe'
+      listingsData
+    }
+    this.change = this.change.bind(this)
+  }
+
+  change(event){
+    console.log(event.target.name)
+  }
+
+  render () {
+    console.log(this.state)
+    return (<div>
+      <Header />
+      <section id="content-area">
+        <Filter change={this.change} />
+        <Listings listingsData={this.state.listingsData} />
+      </section>
+      </div>)
+  }
+}
+
+const app = document.getElementById('app')
+
+ReactDOM.render(<App />, app)
